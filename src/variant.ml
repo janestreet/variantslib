77c24088bb9175336dd27d47a61e43bc4ef82021
Jeremie Dimino <jdimino@janestreet.com>
2016-09-16 15:04:53 +0100

    114.10+84

--- src/variant.ml
+++ src/variant.ml
@@ -1,7 +1,1 @@
-type 'constructor t = {
-  name : string;
-  (* the position of the constructor in the type definition, starting from 0 *)
-  rank : int;
-
-  constructor : 'constructor
-}
+include Base.Variant
--- src/variant.mli
+++
@@ -1,8 +0,0 @@
-(** OCaml variant type. *)
-
-type 'constructor t = {
-  name : string;
-  (* the position of the constructor in the type definition, starting from 0 *)
-  rank : int;
-  constructor : 'constructor
-}
